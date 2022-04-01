9cd58e373e93bbe38f6baa30502014c62111af59
futa125 <ivanfutivic@gmail.com>
2022-04-01 08:21:16 +0200

    Add zad7

---
+++ LAB1/zadatak7.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+dict_file=$1
+
+Help()
+{
+   # Display Help
+   echo
+   echo "Dictionary analysis tool"
+   echo "Usage: ${BASH_SOURCE[0]} [dictionary file]"
+   echo
+}
+
+DictionaryAnalyzer()
+{
+  cut -c1 "$dict_file" | tr "[:upper:]" "[:lower:]" | sort | uniq -ci | sort -nr
+}
+
+if [ "$dict_file" != "" ]; then
+
+    if [ -f "$dict_file" ]; then
+        DictionaryAnalyzer
+    else
+        echo "Error: File $dict_file does not exists."
+        Help
+    fi
+
+else
+    echo  "Error: Dictionary file not specified."
+    Help
+fi
